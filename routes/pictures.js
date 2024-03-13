641262a009f3f2db22dc084ca0296af60f9abf33
aerichs <aerichs@online.no>
2024-03-13 15:59:58 +0100

    AWS implemented to routes and views

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,9 +2,31 @@ var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 var path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
-router.get("/", function (req, res, next) {
-  const pictures = fs.readdirSync(path.join(__dirname, "../pictures/"));
+router.get("/", async function (req, res, next) {
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: "/",
+    Prefix: "public/",
+  };
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map((x) => x.Key);
+  const pictures = await Promise.all(
+    keys.map(async (key) => {
+      let my_file = await s3
+        .getObject({
+          Bucket: process.env.CYCLIC_BUCKET_NAME,
+          Key: key,
+        })
+        .promise();
+      return {
+        src: Buffer.from(my_file.Body).toString("base64"),
+        name: key.split("/").pop(),
+      };
+    })
+  );
   res.render("pictures", { pictures: pictures });
 });
 
@@ -12,10 +34,18 @@ router.get("/:pictureName", function (req, res, next) {
   res.render("pictureDetails", { picture: req.params.pictureName });
 });
 
-router.post("/", function (req, res, next) {
+router.post("/", async function (req, res, next) {
   const file = req.files.file;
-  fs.writeFileSync(path.join(__dirname, "../pictures/", file.name), file.data);
-  //console.log(req.files);
+  console.log(req.files);
+  await s3
+    .putObject({
+      Body: file.data,
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: "public/" + file.name,
+    })
+    .promise();
+  /*fs.writeFileSync(path.join(__dirname, "../pictures/", file.name), file.data);*/
+
   res.end();
 });
 
